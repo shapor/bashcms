2756ef813e796789fa34ea02f351a0035783f7cc
Shapor Naghibzadeh <shapor@yzf.shapor.com>
2008-12-01 09:28:19 -0800

    Add templated per-page <title> in header. Fix "make clean" to not delete non-managed files.

--- make.sh
+++ make.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
+#
+# bashcms
+# Author: Shapor Naghibzadeh
+#
+# generates tabified html pages from content stubs and template files
 
 tmp=/tmp/$0.$$
 tablist=.templates/order
 header=.templates/header.html
 footer=.templates/footer.html
 middle=.templates/middle.html
+title=$(cat .templates/title 2>/dev/null || echo bashcms)
+
+if [[ $1 == "clean" ]]; then
+	echo "cleaning up generated files..."
+	for name in */.index.html; do
+		echo rm ${name/\/*/}.html
+		rm ${name/\/*/}.html
+	done 2>/dev/null
+	exit
+fi
 
 function fname {
 	local -r name=$1
@@ -31,7 +46,7 @@ for i in $(seq 0 $(( ${#tabs[@]} - 1 ))); do
 	name=${names[$i]}
 
 	pushd $tab >/dev/null|| exit
-	cat ../$header >$tmp
+	<../$header sed "s/<title><\/title>/<title>$title - $name<\/title>/" >$tmp
 
 	for j in $(seq 0 $(( ${#tabs[@]} - 1 ))); do
 		if [[ $i -eq $j ]]; then
